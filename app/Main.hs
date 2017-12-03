30ce33c0b255a6e1c6075a79263f30bb5f141994
Marc de Jonge <marcdejonge@gmail.com>
2017-12-03 12:56:00 +0100

    Solved Day 1

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,8 @@ module Main where
 import qualified Day1
 
 main :: IO ()
-main = putStrLn $ show Day1.run
+main = do day1 <- Day1.run
+          putStrLn (writeDayResults 1 day1)
+
+writeDayResults :: (Show a, Show b) => Int -> (a, b) -> String
+writeDayResults day (part1, part2) = "Results of day " ++ (show day) ++ ", part1: " ++ (show part1) ++ " part2: " ++ (show part2)
--- src/Day1.hs
+++ src/Day1.hs
@@ -2,5 +2,20 @@ module Day1
     ( run
     ) where
 
-run :: Integer
-run = 1
+import Data.Char
+
+run = do numbers <- readNumbers
+         return (part1 numbers, part2 numbers)
+
+readNumbers :: IO [Int]
+readNumbers = map digitToInt <$> readFile "data/day1.txt"
+
+part1 :: [Int] -> Int
+part1 numbers = sum $ map saveSame $ zip numbers $ tail numbers ++ [head numbers]
+
+saveSame (x, y)
+    | x == y    = x
+    | otherwise = 0
+
+part2 :: [Int] -> Int
+part2 numbers = 2 * (sum $ map saveSame $ uncurry zip $ splitAt ((length numbers + 1) `div` 2) numbers)
