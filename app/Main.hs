7963551a8652e01a2c4a6766a3167114c1105565
Marc de Jonge <marcdejonge@gmail.com>
2017-12-03 13:11:56 +0100

    Added code to read from which day to start

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,24 @@
 module Main where
 
+import System.IO
 import qualified Day1
 
+prompt msg = do putStr msg
+                hFlush stdout
+                getLine
+
 main :: IO ()
-main = do day1 <- Day1.run
-          putStrLn (writeDayResults 1 day1)
+main = do day <- prompt "Which day do you want to run? "
+          result <- runDay $ read day
+          putStrLn result
+
+runDay :: Int -> IO String
+runDay day = writeDayResults 1 <$> mapNr day
+
+mapNr day =
+    case day of
+        1 -> Day1.run
+        _ -> fail "Unknown day"
 
 writeDayResults :: (Show a, Show b) => Int -> (a, b) -> String
 writeDayResults day (part1, part2) = "Results of day " ++ (show day) ++ ", part1: " ++ (show part1) ++ " part2: " ++ (show part2)
