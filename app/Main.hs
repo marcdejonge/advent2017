0dc2fb3642ad0b8d1711d7834b203c859ac30472
Marc de Jonge <marc@zuludesk.com>
2017-12-05 12:34:01 +0100

    Solved day 5

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,13 @@
 module Main where
 
 import System.IO
+import Data.Time.Clock
+import Text.Printf
 import qualified Day1
 import qualified Day2
 import qualified Day3
 import qualified Day4
+import qualified Day5
 
 prompt msg = do putStr msg
                 hFlush stdout
@@ -12,8 +15,12 @@ prompt msg = do putStr msg
 
 main :: IO ()
 main = do day <- prompt "Which day do you want to run? "
+          start <- getCurrentTime
           result <- runDay $ read day
           putStrLn result
+          end <- getCurrentTime
+          let diff = diffUTCTime end start
+          printf "Computation time: %s\n" (show diff)
 
 runDay :: Int -> IO String
 runDay day = writeDayResults day <$> mapNr day
@@ -24,6 +31,7 @@ mapNr day =
         2 -> showPair <$> Day2.run
         3 -> return $ showPair Day3.run
         4 -> showPair <$> Day4.run
+        5 -> showPair <$> Day5.run
         _ -> fail "Unknown day"
 
 showPair :: (Show a, Show b) => (a, b) -> (String, String)
---
+++ src/Day5.hs
@@ -0,0 +1,28 @@
+module Day5(run) where
+
+import Data.List.Split
+import Control.Monad.ST
+import Data.Array.IO
+import Data.Array.ST
+
+run = do numbers <- readNumbers
+         return (part1 numbers, part2 numbers)
+
+readNumbers :: IO [Int]
+readNumbers = (map read . splitOn "\n") <$> readFile "data/day5.txt"
+
+part1 = calcSteps (+1)
+
+part2 = calcSteps (\x -> if x >= 3 then x - 1 else x + 1)
+
+calcSteps :: (Int -> Int) -> [Int] -> Int
+calcSteps update numbers = runST $ do 
+           array <- newListArray (1, length numbers) numbers :: ST s (STUArray s Int Int)
+           step update 1 array (length numbers) 0
+
+step update ix array length accum
+  | ix > length = return accum
+  | ix <= 0     = fail "ix <= 0"
+  | otherwise   = do curr <- readArray array ix
+                     writeArray array ix (update curr)
+                     step update (ix + curr) array length (accum + 1)
